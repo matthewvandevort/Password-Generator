ef49f62f1022a93bcbc17e1db3879a55be516787
Matthew Vandevort <vandevortmatthew@gmail.com>
2021-06-16 00:43:28 -0400

    gave users the ability to choose character types.

--- Develop/script.js
+++ Develop/script.js
@@ -4,11 +4,37 @@ function generatePassword() {
   var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
   var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
+// Choosing a password length between 8-128 characters long and checking if the number chossen fall between said range.
 
   numberOfCharacters = prompt('How many characters would you like your password to be? Please choose between 8-128 characters in length.');
   if (numberOfCharacters < 8 || numberOfCharacters > 128) {
     return "Please choose a number between 8 and 128.";
+  } else {
+    alert('Your password is ' + numberOfCharacters + ' characters long.');
+
   }
+
+  wantLowerCase = confirm('Would you like lowercase letters in your password? Press OK for yes, Cancel for no.');
+  if (wantLowerCase) {
+    alert('Your password will have lowercase letters.');
+  } else {
+    alert('Your password will NOT have lowercase letters.');
+  }   
+  
+  wantUpperCase = confirm('Would you like lowercase letters in your password? Press OK for yes, Cancel for no.');
+  if (wantUpperCase) {
+    alert('Your password will have uppercase letters.');
+  } else {
+    alert('Your password will NOT have uppercase letters.');
+  }   
+  
+  wantNumbers = confirm('Would you like numbers in your password? Press OK for yes, Cancel for no.');
+  if (wantNumbers) {
+    alert('Your password will have numbers.');
+  } else {
+    alert('Your password will NOT have numbers.');
+  }   
+
 }
 var generateBtn = document.querySelector("#generate");
 
