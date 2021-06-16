b58000f702364ecd3085a5c62a38c01995ff0d55
Matthew Vandevort <vandevortmatthew@gmail.com>
2021-06-16 15:03:49 -0400

    added for loop for final password and commented js

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,14 @@
 // Assignment Code
+
+// Function with variables for posssible characters.
 function generatePassword() {
-  var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+  var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
   var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
   var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  
   var possiblePassword = [];
 
-// Choosing a password length between 8-128 characters long and checking if the number chossen fall between said range.
+// Choosing a password length between 8-128 characters long. Checking if the number chossen fall between said range.
 
   var numberOfCharacters = prompt('How many characters would you like your password to be? Please choose between 8-128 characters in length.');
   if (numberOfCharacters < 8 || numberOfCharacters > 128) {
@@ -14,6 +17,8 @@ function generatePassword() {
     alert('Your password is ' + numberOfCharacters + ' characters long.');
 
   }
+
+// Checking if the user wants each character type in their password.
 
   var wantLowerCase = confirm('Would you like lowercase letters in your password? Press OK for yes, Cancel for no.');
   if (wantLowerCase === true) {
@@ -36,11 +41,14 @@ function generatePassword() {
     alert('Your password will NOT have numbers.');
   }
   
+// If users don't select at least one character type, return a error message.
+
   if (wantLowerCase === false && wantUpperCase === false && wantNumbers === false) {
     return'Please select at least one valid character type!!!';
   };
 
-  
+  // If users select a certain character type it gets added to the empty array in the variable 'possiblePassword'.
+
   if (wantLowerCase === true) {
     var possiblePassword = possiblePassword.concat(lowercase);
   }
@@ -53,8 +61,17 @@ function generatePassword() {
     var possiblePassword = possiblePassword.concat(numbers);
   }
 
+// Using a for loop to generate a random password with the Math.floor and Math.random methods which
 
+  let finalPassword = ""
+  for (let i = 0; i < numberOfCharacters; i++) {
+    let randomNumberGenerator = [Math.floor(Math.random() * possiblePassword.length)];
 
+    finalPassword = finalPassword + possiblePassword[randomNumberGenerator];
+  }
+
+  // Returns the final password to the screen.
+  return finalPassword;
 
 };
 
