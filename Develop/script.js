0d9cfb368d98680f98edecb3a538336ff2305675
Matthew Vandevort <vandevortmatthew@gmail.com>
2021-06-16 01:58:39 -0400

    added if statements to check if users selected a character type

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,13 @@
 // Assignment Code
 function generatePassword() {
   var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
-  var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-  var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  var possiblePassword = [];
 
 // Choosing a password length between 8-128 characters long and checking if the number chossen fall between said range.
 
-  numberOfCharacters = prompt('How many characters would you like your password to be? Please choose between 8-128 characters in length.');
+  var numberOfCharacters = prompt('How many characters would you like your password to be? Please choose between 8-128 characters in length.');
   if (numberOfCharacters < 8 || numberOfCharacters > 128) {
     return "Please choose a number between 8 and 128.";
   } else {
@@ -14,28 +15,51 @@ function generatePassword() {
 
   }
 
-  wantLowerCase = confirm('Would you like lowercase letters in your password? Press OK for yes, Cancel for no.');
-  if (wantLowerCase) {
+  var wantLowerCase = confirm('Would you like lowercase letters in your password? Press OK for yes, Cancel for no.');
+  if (wantLowerCase === true) {
     alert('Your password will have lowercase letters.');
   } else {
     alert('Your password will NOT have lowercase letters.');
   }   
   
-  wantUpperCase = confirm('Would you like lowercase letters in your password? Press OK for yes, Cancel for no.');
-  if (wantUpperCase) {
+  var wantUpperCase = confirm('Would you like lowercase letters in your password? Press OK for yes, Cancel for no.');
+  if (wantUpperCase === true) {
     alert('Your password will have uppercase letters.');
   } else {
     alert('Your password will NOT have uppercase letters.');
   }   
   
-  wantNumbers = confirm('Would you like numbers in your password? Press OK for yes, Cancel for no.');
-  if (wantNumbers) {
+  var wantNumbers = confirm('Would you like numbers in your password? Press OK for yes, Cancel for no.');
+  if (wantNumbers === true) {
     alert('Your password will have numbers.');
   } else {
     alert('Your password will NOT have numbers.');
-  }   
+  }
+  
+  if (wantLowerCase === false && wantUpperCase === false && wantNumbers === false) {
+    return'Please select at least one valid character type!!!';
+  };
 
-}
+  
+  if (wantLowerCase === true) {
+    var possiblePassword = possiblePassword.concat(lowercase);
+  }
+  
+  if (wantUpperCase === true) {
+    var possiblePassword = possiblePassword.concat(uppercase);
+  }
+  
+  if (wantNumbers === true) {
+    var possiblePassword = possiblePassword.concat(numbers);
+  }
+
+
+
+
+};
+
+
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
